540ae87e03d2f4f7afcc4b412104d6157adec51c
kovalsky <42189932+klinoshkov@users.noreply.github.com>
2025-04-06 16:58:04 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -58,13 +58,13 @@ const Footer = () => {
       </div>
 
       <div className="w-full bg-white py-4 flex flex-wrap items-center justify-center gap-6">
-        <img src="/visa.png" alt="Visa" className="h-8 object-contain" />
-        <img src="/mastercard.png" alt="Mastercard" className="h-8 object-contain" />
-        <img src="/troy.png" alt="Troy" className="h-8 object-contain" />
-        <img src="/jcb.png" alt="JCB" className="h-8 object-contain" />
-        <img src="/unionpay.png" alt="UnionPay" className="h-8 object-contain" />
-        <img src="/bank-transfer.png" alt="Bank Transfer" className="h-8 object-contain" />
-        <img src="/havale.png" alt="Havale" className="h-8 object-contain" />
+        <img src="/visa.png" alt="Visa" className="h-8 w-auto object-contain px-2" />
+        <img src="/mastercard.png" alt="Mastercard" className="h-8 w-auto object-contain px-2" />
+        <img src="/troy.png" alt="Troy" className="h-8 w-auto object-contain px-2" />
+        <img src="/jcb.png" alt="JCB" className="h-8 w-auto object-contain px-2" />
+        <img src="/unionpay.png" alt="UnionPay" className="h-8 w-auto object-contain px-2" />
+        <img src="/bank-transfer.png" alt="Bank Transfer" className="h-8 w-auto object-contain px-2" />
+        <img src="/havale.png" alt="Havale" className="h-8 w-auto object-contain px-2" />
       </div>
     </footer>
   );
